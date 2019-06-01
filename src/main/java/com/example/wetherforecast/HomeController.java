917f7b677d6fee9ccca86b17305fae0e104f4fa5
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-01 11:59:34 +0200

    second commit

--- src/main/java/com/example/wetherforecast/HomeController.java
+++ src/main/java/com/example/wetherforecast/HomeController.java
@@ -1,4 +1,15 @@
 package com.example.wetherforecast;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public  String home(){
+        return "home";          //z pliku html ktory znajduje sie w folderze templates
+
+    }
+
 }
