68eedc201594a0f98f4c034dbe6d001570e1cc11
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-01 11:39:29 +0200

    first commit

---
+++ src/main/java/com/example/wetherforecast/WetherforecastApplication.java
@@ -0,0 +1,13 @@
+package com.example.wetherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetherforecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WetherforecastApplication.class, args);
+    }
+
+}
