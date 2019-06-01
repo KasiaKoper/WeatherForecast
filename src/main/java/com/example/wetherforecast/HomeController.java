91c27a18115acbc94b3f28d3fc893e9b89b1a300
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-01 12:18:13 +0200

    added home controller

--- src/main/java/com/example/wetherforecast/HomeController.java
+++ src/main/java/com/example/wetherforecast/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.wetherforecast;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public  String home(){
+    @GetMapping("/")        //get mapping bo pobieramy (metoda get)
+    public  String home(ModelMap map){                  //Model Map gdy chcemy przesłać dane z Javy do html
+        map.put("a",20);
         return "home";          //z pliku html ktory znajduje sie w folderze templates
 
     }
